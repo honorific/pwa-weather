899b0d70becf16efc215b665e8cec4de25bc01df
honorific <honorific.info@gmail.com>
2023-05-08 11:34:10 +0330

    show the temprture

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ const App = () => {
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
+          <div className='city-temp'>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;</sup>
+          </div>
         </div>
       )}
     </div>
